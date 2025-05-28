7aca41d93db106a5e3bd408a5680c78a403ed377
jawaharsGitHub <jawahars@live.in>
2025-05-29 05:09:10 +0530

    swagger ui features

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import swaggerUi from 'swagger-ui-express';
 import { remultApi } from 'remult/remult-express';
 import { api } from "./api";
 
@@ -6,6 +7,9 @@ import { api } from "./api";
 const app = express();
 app.use(express.json()); // To parse JSON request bodies
 app.use(api);
+const openApiDocument = api.openApiDoc({ title: "remult-react-todo" });
+app.get("/api/openApi.json", (req, res) => {res.json(openApiDocument)});
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
 //const PORT = process.env.PORT || 3000;
 app.listen(3002, () => {
   console.log("Server is running on port 3002");
