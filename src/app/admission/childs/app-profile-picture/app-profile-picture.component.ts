741129339e2a5523823f259d9f869b7244fb2957
jawaharsGitHub <jawahars@live.in>
2025-06-01 00:31:45 +0530

    filter only images

--- src/app/admission/childs/app-profile-picture/app-profile-picture.component.ts
+++ src/app/admission/childs/app-profile-picture/app-profile-picture.component.ts
@@ -16,13 +16,13 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [NgIf, FormsModule],
   templateUrl: './app-profile-picture.component.html',
-  styleUrl: './app-profile-picture.component.scss',
+  styleUrl: './app-profile-picture.component.css',
 })
 export class AppProfilePictureComponent {
   profilePicture: WritableSignal<Aviator[]> = signal([]); // Signal holding an array of Aviator objects
   @Output() profilePictureChange = new EventEmitter<Aviator>();
   @Output() stepValidity = new EventEmitter<boolean>();
-  
+
   onFileSelected(event: any): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
