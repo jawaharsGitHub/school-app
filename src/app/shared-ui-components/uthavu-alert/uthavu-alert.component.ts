ef4d7c646c12bb381ba16ee1b726eb8bc1a35acd
jawaharsGitHub <jawahars@live.in>
2025-06-01 16:26:48 +0530

    shared-ui-components for project structure only

---
+++ src/app/shared-ui-components/uthavu-alert/uthavu-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uthavu-alert',
+  standalone: true,
+  imports: [],
+  templateUrl: './uthavu-alert.component.html',
+  styleUrl: './uthavu-alert.component.css'
+})
+export class UthavuAlertComponent {
+
+}
