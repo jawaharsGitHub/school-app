a02c7b5f55ce5c24ae25954b12ecf83d1d287cdd
suganya ramakrishnan <yuvanji555@gmail.com>
2025-06-07 21:30:03 +0530

    css changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { MainComponent } from "./features/main/main.component";
 import { AdmissionListComponent } from "./features/admission/admission-list/admission-list.component";
 import { AdmissionMainComponent } from "./features/admission/admission-form-main/admission-form-main.component";
 import { ApplicationFormComponent } from "./features/admission/application-form/application-form.component";
-import { DashboardSuganyaComponent } from "./dashboard-suganya/dashboard-suganya.component";
+import { DashboardSuganyaComponent } from "./dashboard/dashboard-suganya/dashboard-suganya.component";
 
 export const routes: Routes = [
   { path: 'login', component: SignInComponent },
@@ -16,13 +16,13 @@ export const routes: Routes = [
     component: MasterComponent,
     //canActivate: [AuthGuard], // Optional: protect access, if we enable this, remult.user is undefined
     children: [
-      { path: 'dashboard', component: MainComponent },
+      { path: 'dashboard1', component: MainComponent },
       { path: 'admin/applicants', component: AdmissionListComponent },
       { path: 'admin/applicants/new', component: AdmissionMainComponent },
       { path: 'admin/applicants/edit/:id', component: AdmissionMainComponent },
       { path: 'admin/application/new', component: ApplicationFormComponent },
       { path: 'admin/application/edit/:id', component: ApplicationFormComponent }, 
-      { path: 'admin/dashboard-suganya', component: DashboardSuganyaComponent }, 
+      { path: 'dashboard', component: DashboardSuganyaComponent }, 
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
     ]
   },
--- src/app/dashboard/dashboard-suganya/dashboard-suganya.component.ts
+++ src/app/dashboard/dashboard-suganya/dashboard-suganya.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { InfoCardComponent } from '../info-card/info-card.component';
 
 @Component({
   selector: 'app-dashboard-suganya',
   standalone: true,
-  imports: [],
+   imports: [InfoCardComponent],  // Correct use of imports for standalone component
   templateUrl: './dashboard-suganya.component.html',
   styleUrl: './dashboard-suganya.component.css'
 })
